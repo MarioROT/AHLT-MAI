39e0fcbf07a497fe710b5f9131d7b1fc9e564ea4
kszczep4 <01149444@pw.edu.pl>
2024-05-10 16:32:31 +0200

    uploading test stats

--- Session 4/run.sh
+++ Session 4/run.sh
@@ -8,21 +8,27 @@ AHLT=../
 # python extract-features.py $AHLT/data/devel/ > devel.feat &
 # echo "Extracting features train..."
 # python extract-features.py $AHLT/data/train/ > train.feat
-echo "Extracting features test..."
-python extract-features.py $AHLT/data/test/ > test.feat
+# echo "Extracting features test..."
+# python extract-features.py $AHLT/data/test/ > test.feat
 
 # # kill `cat /tmp/corenlp-server.running`
 
 # ############################################
-# # train MEM model
-# echo "Training MEM model..."
-# python train.py train.feat model.mem C=10
+# train MEM model
+echo "Training MEM model..."
+python train.py train.feat model.mem C=10
 # # run MEM model
-# echo "Running MEM model..."
-# python predict.py devel.feat model.mem > devel-MEM.out
+echo "Running MEM model..."
+python predict.py devel.feat model.mem > devel-MEM.out
+# evaluate MEM results
+echo "Evaluating MEM results..."
+python $AHLT/util/evaluator.py DDI $AHLT/data/devel devel-MEM.out > devel-MEM.stats
+
+# echo "Running MEM model on test..."
+# python predict.py test.feat model.mem > test-MEM.out
 # # evaluate MEM results
 # echo "Evaluating MEM results..."
-# python $AHLT/util/evaluator.py DDI $AHLT/data/devel devel-MEM.out > devel-MEM.stats
+# python $AHLT/util/evaluator.py DDI $AHLT/data/test test-MEM.out > test-MEM.stats
 # ############################################
 
 # train SVM model
@@ -35,11 +41,11 @@ python extract-features.py $AHLT/data/test/ > test.feat
 # echo "Evaluating SVM results..."
 # python $AHLT/util/evaluator.py DDI $AHLT/data/devel devel-SVM.out > devel-SVM.stats
 
-echo "Running SVM model on test..."
-python predict.py test.feat model.svm > test-SVM.out
-# evaluate SVM results
-echo "Evaluating SVM results..."
-python $AHLT/util/evaluator.py DDI $AHLT/data/test test-SVM.out > test-SVM.stats
+# echo "Running SVM model on test..."
+# python predict.py test.feat model.svm > test-SVM.out
+# # evaluate SVM results
+# echo "Evaluating SVM results..."
+# python $AHLT/util/evaluator.py DDI $AHLT/data/test test-SVM.out > test-SVM.stats
 
 ############################################
 
