cdbf0fc66d963b752e5aab42a9f456304e50b3fd
kszczep4 <01149444@pw.edu.pl>
2024-06-15 16:20:11 +0200

    added bert stats

--- Session 5/codemaps.py
+++ Session 5/codemaps.py
@@ -95,7 +95,8 @@ class Codemaps :
             for key in self.suf_index : print('SUF', key, self.suf_index[key], file=f)
 
 
-    ## --------- encode X from given data ----------- 
+    ## --------- encode X from given data -----------
+    # ORIGINAL 
     # def encode_words(self, data) :
 
     #     # encode sentence words
@@ -133,59 +134,6 @@ class Codemaps :
     #     # return encoded sequences
     #     return [Xl,Xw,Xs,Xf]
     #     # return [Xw,Xs]
-    ###############################################################################################
-    # def encode_words(self, data):
-    #     # encode sentence words (including lowercase)
-    #     enc_words = [
-    #         torch.Tensor([
-    #             self.word_index[w['form']] if w['form'] in self.word_index else self.word_index['UNK'] 
-    #             for w in s
-    #         ]) for s in data.sentences()
-    #     ]
-        
-    #     enc_lower = [
-    #         torch.Tensor([
-    #             self.word_index[w['form'].lower()] if w['form'].lower() in self.word_index else self.word_index['UNK']
-    #             for w in s
-    #         ]) for s in data.sentences()
-    #     ]
-
-    #     # truncate sentences longer than maxlen
-    #     enc_words = [s[0:self.maxlen] for s in enc_words]
-    #     enc_lower = [s[0:self.maxlen] for s in enc_lower]
-
-    #     # Create padding tensors for words and lowercased words
-    #     Xw = torch.full((len(enc_words), self.maxlen), self.word_index['PAD'], dtype=torch.int64)
-    #     Xl = torch.full((len(enc_lower), self.maxlen), self.word_index['PAD'], dtype=torch.int64)
-    #     for i, s in enumerate(enc_words):
-    #         Xw[i, 0:len(s)] = s
-    #     for i, s in enumerate(enc_lower):
-    #         Xl[i, 0:len(s)] = s
-
-
-    #     # encode sentence suffixes
-    #     enc = [torch.Tensor([self.suf_index[w['lc_form'][-self.suflen:]] if w['lc_form'][-self.suflen:] in self.suf_index else self.suf_index['UNK'] for w in s]) for s in data.sentences()]
-    #     # cut sentences longer than maxlen
-    #     enc = [s[0:self.maxlen] for s in enc]
-    #     # create a tensor full of padding
-    #     tsr = torch.Tensor([])
-    #     Xs = tsr.new_full((len(enc), self.maxlen), self.suf_index['PAD'], dtype=torch.int64)
-
-    #     # Additional features as separate tensors
-    #     Xcap = torch.zeros((len(enc_words), self.maxlen), dtype=torch.int64)  # Capitalization
-    #     Xdash = torch.zeros((len(enc_words), self.maxlen), dtype=torch.int64)  # Dashes
-    #     Xnum = torch.zeros((len(enc_words), self.maxlen), dtype=torch.int64)  # Numbers
-    #     Xext = torch.zeros((len(enc_words), self.maxlen), dtype=torch.int64)  # External presence
-    #     for i, s in enumerate(data.sentences()):
-    #         for j, w in enumerate(s):
-    #             if j < self.maxlen:
-    #                 Xcap[i, j] = w['form'][0].isupper()  # Capitalization
-    #                 Xdash[i, j] = '-' in w['form']  # Dashes
-    #                 Xnum[i, j] = any(char.isdigit() for char in w['form'])  # Numbers
-
-    #     # Return the encoded sequences
-    #     return [Xw, Xs, Xl, Xcap, Xdash, Xnum]
-    ###############################################################################################
 
     def get_external(self, word):
         return self.entity_to_index.get(self.external.get(word.lower(), 'NONE'))
@@ -297,5 +245,178 @@ class Codemaps :
                 return l
         raise KeyError
 
+
+# #####################################################
+# # FOR bert
+# import os
+# import string
+# import re
+# import torch
+# from transformers import BertTokenizer
+
+
+# from dataset import *
+
+# class Codemaps :
+#     # --- constructor, create mapper either from training data, or
+#     # --- loading codemaps from given file
+#     def __init__(self, data, maxlen=None, suflen=None):
+#         self.external = {}
+#         with open("../resources/HSDB.txt", encoding="utf-8") as h:
+#             for x in h.readlines():
+#                 self.external[x.strip().lower()] = "drug"
+#         with open("../resources/DrugBank.txt", encoding="utf-8") as h:
+#             for x in h.readlines():
+#                 (n, t) = x.strip().lower().split("|")
+#                 self.external[n] = t
+
+#         self.entity_to_index = {"NONE": 0, "drug": 1, "drug_n": 2, "brand": 3, "group": 4}
+
+#         if isinstance(data, Dataset) and maxlen is not None and suflen is not None:
+#             self.__create_indexs(data, maxlen, suflen)
+#         elif type(data) == str and maxlen is None and suflen is None:
+#             self.__load(data)
+#         else:
+#             print('codemaps: Invalid or missing parameters in constructor')
+#             exit()
+
+#         self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+
+ 
+
+            
+#     # --------- Create indexs from training data
+#     # Extract all words and labels in given sentences and 
+#     # create indexes to encode them as numbers when needed
+#     def __create_indexs(self, data, maxlen, suflen) :
+
+#         self.maxlen = maxlen
+#         self.suflen = suflen
+#         words = set([])
+#         sufs = set([])
+#         labels = set([])
+        
+#         for s in data.sentences() :
+#             for t in s :
+#                 words.add(t['form'])
+#                 sufs.add(t['lc_form'][-self.suflen:])
+#                 labels.add(t['tag'])
+
+#         self.word_index = {w: i+2 for i,w in enumerate(list(words))}
+#         self.word_index['PAD'] = 0 # Padding
+#         self.word_index['UNK'] = 1 # Unknown words
+
+#         self.suf_index = {s: i+2 for i,s in enumerate(list(sufs))}
+#         self.suf_index['PAD'] = 0  # Padding
+#         self.suf_index['UNK'] = 1  # Unknown suffixes
+
+#         self.label_index = {t: i+1 for i,t in enumerate(list(labels))}
+#         self.label_index['PAD'] = 0 # Padding
+        
+#     ## --------- load indexs ----------- 
+#     def __load(self, name) : 
+#         self.maxlen = 0
+#         self.suflen = 0
+#         self.word_index = {}
+#         self.suf_index = {}
+#         self.label_index = {}
+
+#         with open(name+".idx") as f :
+#             for line in f.readlines(): 
+#                 (t,k,i) = line.split()
+#                 if t == 'MAXLEN' : self.maxlen = int(k)
+#                 elif t == 'SUFLEN' : self.suflen = int(k)                
+#                 elif t == 'WORD': self.word_index[k] = int(i)
+#                 elif t == 'SUF': self.suf_index[k] = int(i)
+#                 elif t == 'LABEL': self.label_index[k] = int(i)
+                            
+    
+#     ## ---------- Save model and indexs ---------------
+#     def save(self, name) :
+#         # save indexes
+#         with open(name+".idx","w") as f :
+#             print ('MAXLEN', self.maxlen, "-", file=f)
+#             print ('SUFLEN', self.suflen, "-", file=f)
+#             for key in self.label_index : print('LABEL', key, self.label_index[key], file=f)
+#             for key in self.word_index : print('WORD', key, self.word_index[key], file=f)
+#             for key in self.suf_index : print('SUF', key, self.suf_index[key], file=f)
+
+
+#     def get_external(self, word):
+#         return self.entity_to_index.get(self.external.get(word.lower(), 'NONE'))
+
+
+#     def encode_words(self, data):
+#         sentences = [sentence for sentence in data.sentences()]
+#         tokens = [self.tokenizer.tokenize(' '.join([w['form'] for w in s])) for s in sentences]
+#         token_ids = [self.tokenizer.convert_tokens_to_ids(t) for t in tokens]
+
+#         # Truncate sentences longer than maxlen
+#         token_ids = [t[:self.maxlen] for t in token_ids]
+
+#         # Additional features as separate tensors
+#         Xcap = torch.zeros((len(token_ids), self.maxlen), dtype=torch.int64)
+#         Xdash = torch.zeros((len(token_ids), self.maxlen), dtype=torch.int64)
+#         Xnum = torch.zeros((len(token_ids), self.maxlen), dtype=torch.int64)
+#         Xext = torch.zeros((len(token_ids), self.maxlen), dtype=torch.int64)
+#         Xspecial = torch.zeros((len(token_ids), self.maxlen), dtype=torch.int64)
+#         Xlen = torch.zeros((len(token_ids), self.maxlen), dtype=torch.int64)
+#         Xpos = torch.zeros((len(token_ids), self.maxlen), dtype=torch.int64)
+
+#         for i, s in enumerate(sentences):
+#             for j, w in enumerate(s):
+#                 if j < self.maxlen:
+#                     Xcap[i, j] = w['form'][0].isupper()  # Capitalization
+#                     Xdash[i, j] = '-' in w['form']  # Dashes
+#                     Xnum[i, j] = any(char.isdigit() for char in w['form'])  # Numbers
+#                     Xext[i, j] = self.get_external(w['form'])  # External presence as integer
+#                     Xspecial[i, j] = any(char in '%@#&$*' for char in w['form'])  # Special characters
+#                     Xlen[i, j] = len(w['form'])  # Length of word
+#                     Xpos[i, j] = j  # Position in sentence
+
+#         # Convert to tensors and pad
+#         token_ids = [torch.tensor(t) for t in token_ids]
+#         token_ids = torch.nn.utils.rnn.pad_sequence(token_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id)
+
+#         return [token_ids, Xcap, Xdash, Xnum, Xext, Xspecial, Xlen, Xpos]
+#     ## --------- encode Y from given data ----------- 
+#     def encode_labels(self, data) :
+#         # encode and pad sentence labels
+#         enc = [torch.Tensor([self.label_index[w['tag']] for w in s]) for s in data.sentences()]
+#         # cut sentences longer than maxlen
+#         enc = [s[0:self.maxlen] for s in enc]
+#         # create a tensor full of padding
+#         tsr = torch.Tensor([])
+#         Y = tsr.new_full((len(enc), self.maxlen), self.label_index['PAD'], dtype=torch.int64)
+#         # fill padding tensor with sentence data
+#         for i, s in enumerate(enc): Y[i, 0:s.size()[0]] = s
+               
+#         return Y
+
+#     ## -------- get word index size ---------
+#     def get_n_words(self) :
+#         return len(self.word_index)
+#     ## -------- get suf index size ---------
+#     def get_n_sufs(self) :
+#         return len(self.suf_index)
+#     ## -------- get label index size ---------
+#     def get_n_labels(self) :
+#         return len(self.label_index)
+
+#     ## -------- get index for given word ---------
+#     def word2idx(self, w) :
+#         return self.word_index[w]
+#     ## -------- get index for given suffix --------
+#     def suff2idx(self, s) :
+#         return self.suff_index[s]
+#     ## -------- get index for given label --------
+#     def label2idx(self, l) :
+#         return self.label_index[l]
+#     ## -------- get label name for given index --------
+#     def idx2label(self, i) :
+#         for l in self.label_index :
+#             if self.label_index[l] == i:
+#                 return l
+#         raise KeyError
 
 
--- Session 5/network.py
+++ Session 5/network.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from transformers import BertModel, BertTokenizer
 
 criterion = nn.CrossEntropyLoss()
 
+
+## Original network
 # class nercLSTM(nn.Module):
 #    def __init__(self, codes) :
 #       super(nercLSTM, self).__init__()
@@ -32,7 +34,8 @@ criterion = nn.CrossEntropyLoss()
 #       x = self.out(x)
 #       return x
 
-
+################################################################
+## Bigger network with more inputs
 class nercLSTM(nn.Module):
     def __init__(self, codes):
         super(nercLSTM, self).__init__()
@@ -48,323 +51,88 @@ class nercLSTM(nn.Module):
         self.dropS = nn.Dropout(0.1)
         self.dropL = nn.Dropout(0.1)  # Dropout for lowercased words
 
-        # Additional feature dimensions
         additional_feature_dim = 7  # Xcap, Xdash, Xnum, Xext, Xspecial, Xlen, Xpos
 
-        # LSTM layers for word and suffix embeddings
         self.lstm_word = nn.LSTM(100, 100, bidirectional=True, batch_first=True)
         self.lstm_suf = nn.LSTM(50, 50, bidirectional=True, batch_first=True)
         self.lstm_lower = nn.LSTM(100, 100, bidirectional=True, batch_first=True)  # LSTM for lowercased words
 
-        # LSTM for combined features (updated input size)
         combined_input_size = 200 + 100 + 200 + additional_feature_dim  # Corrected combined input size
         self.lstm_combined = nn.LSTM(combined_input_size, 200, bidirectional=True, batch_first=True)
 
-        # Fully connected layers
         self.fc1 = nn.Linear(400, 200)
         self.fc2 = nn.Linear(200, 100)
-        
-        # Output layer
         self.out = nn.Linear(100, n_labels)
 
     def forward(self, w, s, l, Xcap, Xdash, Xnum, Xext, Xspecial, Xlen, Xpos):
-        # Word embeddings
         x = self.embW(w)
         x = self.dropW(x)
         x, _ = self.lstm_word(x)
 
-        # Suffix embeddings
         y = self.embS(s)
         y = self.dropS(y)
         y, _ = self.lstm_suf(y)
 
-        # Lowercased word embeddings
         z = self.embL(l)
         z = self.dropL(z)
         z, _ = self.lstm_lower(z)
 
-        # Concatenate the word, suffix, and lowercased word LSTM outputs
         combined_embeddings = torch.cat((x, y, z), dim=2)
 
-        # Concatenate the additional features
         additional_features = torch.stack((Xcap, Xdash, Xnum, Xext, Xspecial, Xlen, Xpos), dim=2).float()
         combined_features = torch.cat((combined_embeddings, additional_features), dim=2)
 
-        # Combined LSTM
         combined_features, _ = self.lstm_combined(combined_features)
-        
-        # Fully connected layers
+
         fc_output = torch.relu(self.fc1(combined_features))
         fc_output = torch.relu(self.fc2(fc_output))
 
-        # Output layer
         output = self.out(fc_output)
         
         return output
 
-
-
-# class nercLSTM(nn.Module):
-#    def __init__(self, codes):
-#       super(nercLSTM, self).__init__()
-
-#       n_words = codes.get_n_words()
-#       n_sufs = codes.get_n_sufs()
-#       n_labels = codes.get_n_labels()
-      
-#       self.embW = nn.Embedding(n_words, 100)
-#       self.embS = nn.Embedding(n_sufs, 50)
-#       self.dropW = nn.Dropout(0.1)
-#       self.dropS = nn.Dropout(0.1)
-
-#       self.lstm1 = nn.LSTM(150, 200, bidirectional=True, batch_first=True)
-#       self.lstm2 = nn.LSTM(400, 200, bidirectional=True, batch_first=True)
-      
-#       self.out = nn.Linear(400, n_labels)
-
-#    def forward(self, w, s):
-#       x = self.embW(w)
-#       y = self.embS(s)
-#       x = self.dropW(x)
-#       y = self.dropS(y)
-
-#       x = torch.cat((x, y), dim=2)
-#       x, _ = self.lstm1(x)  # First LSTM layer
-#       x, _ = self.lstm2(x)  # Second LSTM layer
-#       x = self.out(x)
-#       return x
-
+############################################################
+## Network with Bert embeddings
 
 # class nercLSTM(nn.Module):
-#     def __init__(self, codes):
+#     def __init__(self, codes, freeze_bert=False):
 #         super(nercLSTM, self).__init__()
 
-#         n_words = codes.get_n_words()
-#         n_sufs = codes.get_n_sufs()
+#         self.bert = BertModel.from_pretrained('bert-base-cased')
+#         self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+        
+#         # if freeze_bert:
+#         #     for param in self.bert.parameters():
+#         #         param.requires_grad = False
+        
 #         n_labels = codes.get_n_labels()
-        
-#         # Embeddings
-#         self.embW = nn.Embedding(n_words, 100)  # Main words
-#         self.embS = nn.Embedding(n_sufs, 50)    # Suffixes
-#         self.embL = nn.Embedding(n_words, 100)  # Lowercase words
-        
-#         # Dropout
-#         self.dropW = nn.Dropout(0.1)
-#         self.dropS = nn.Dropout(0.1)
-#         self.dropL = nn.Dropout(0.1)
+#         additional_feature_dim = 7  # Xcap, Xdash, Xnum, Xext, Xspecial, Xlen, Xpos
 
-#         # LSTM Layers
-#         self.lstm1 = nn.LSTM(404, 200, bidirectional=True, batch_first=True)  # Updated input size
-#         self.lstm2 = nn.LSTM(400, 200, bidirectional=True, batch_first=True)
-        
-#         # Output layer
-#         self.out = nn.Linear(400, n_labels)
+#         # LSTM for combined features (updated input size)
+#         combined_input_size = 768 + additional_feature_dim  # 768 is the BERT hidden size
+#         self.lstm_combined = nn.LSTM(combined_input_size, 200, bidirectional=True, batch_first=True)
+#         self.dropout = nn.Dropout(0.5)  # Adding dropout for regularization
 
-#     def forward(self, Xw, Xs, Xl, Xcap, Xdash, Xnum):
-#         xw = self.dropW(self.embW(Xw))    # Embed and apply dropout to main words
-#         xs = self.dropS(self.embS(Xs))    # Embed and apply dropout to suffixes
-#         xl = self.dropL(self.embL(Xl))    # Embed and apply dropout to lowercase words
+#         # Fully connected layers with increased size
+#         self.fc1 = nn.Linear(400, 300)  # Increased from 200 to 300
+#         self.fc2 = nn.Linear(300, 200)  # Increased from 100 to 200
+#         self.fc3 = nn.Linear(200, 100)  # Added a third fully connected layer
+#         self.out = nn.Linear(100, n_labels)  # Adjusted to match the previous layer size
 
-#         # Stack all features along the feature dimension
-#         x = torch.cat((xw, xs, xl, Xcap.unsqueeze(2), Xdash.unsqueeze(2), Xnum.unsqueeze(2)), dim=2)
-        
-#         x, _ = self.lstm1(x)  # First LSTM layer
-#         x, _ = self.lstm2(x)  # Second LSTM layer
-#         x = self.out(x)       # Output layer
-        
-#         return x
+#     def forward(self, token_ids, Xcap, Xdash, Xnum, Xext, Xspecial, Xlen, Xpos):
+#         # Get BERT embeddings
+#         bert_outputs = self.bert(token_ids)
+#         x = bert_outputs.last_hidden_state
 
+#         # Concatenate the additional features
+#         additional_features = torch.stack((Xcap, Xdash, Xnum, Xext, Xspecial, Xlen, Xpos), dim=2).float()
+#         combined_features = torch.cat((x, additional_features), dim=2)
 
-###########################################################################
-
-# class nercLSTM(nn.Module):
-#     def __init__(self, codes):
-#         super(nercLSTM, self).__init__()
-
-#         n_words = codes.get_n_words()
-#         n_sufs = codes.get_n_sufs()
-#         n_labels = codes.get_n_labels()
-        
-#         # Embeddings
-#         self.embW = nn.Embedding(n_words, 100)  # Main words
-#         self.embS = nn.Embedding(n_sufs, 50)    # Suffixes
-#         self.embL = nn.Embedding(n_words, 100)  # Lowercase words
-        
-#         # Dropout
-#         self.dropW = nn.Dropout(0.1)
-#         self.dropS = nn.Dropout(0.1)
-#         self.dropL = nn.Dropout(0.1)
-
-#         # CNN Layer
-#         self.conv = nn.Conv1d(in_channels=253, out_channels=100, kernel_size=3, padding=1)
-#         self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
-        
-#         # Transposed CNN Layer to restore sequence length
-#         self.deconv = nn.ConvTranspose1d(in_channels=100, out_channels=100, kernel_size=2, stride=2)
-
-#         # LSTM Layers
-#         self.lstm1 = nn.LSTM(100, 200, bidirectional=True, batch_first=True)
-#         self.lstm2 = nn.LSTM(400, 200, bidirectional=True, batch_first=True)
-        
-#         # Output layer
-#         self.out = nn.Linear(400, n_labels)
-
-#     def forward(self, Xw, Xs, Xl, Xcap, Xdash, Xnum):
-#         xw = self.dropW(self.embW(Xw))    # Embed and apply dropout to main words
-#         xs = self.dropS(self.embS(Xs))    # Embed and apply dropout to suffixes
-#         xl = self.dropL(self.embL(Xl))    # Embed and apply dropout to lowercase words
-
-#         # Stack all features along the feature dimension
-#         x = torch.cat((xw, xs, xl, Xcap.unsqueeze(2), Xdash.unsqueeze(2), Xnum.unsqueeze(2)), dim=2)  # (batch, seq_length, feature)
-        
-#         # Permute to have channels as the second dimension for CNN layer
-#         x = x.permute(0, 2, 1)  # (batch, feature, seq_length)
-#         x = self.pool(F.relu(self.conv(x)))  # Apply CNN and pooling
-        
-#         # Restore the sequence length using transposed convolution
-#         x = self.deconv(x)  # Apply transposed CNN
-        
-#         # Permute back for LSTM layer (batch, seq_length, feature)
-#         x = x.permute(0, 2, 1)
-        
-#         x, _ = self.lstm1(x)  # First LSTM layer
-#         x, _ = self.lstm2(x)  # Second LSTM layer
-#         x = self.out(x)       # Output layer
-        
-#         return x
-
-# # 52 on devel
-# class nercLSTM(nn.Module):
-#     def __init__(self, codes, pretrained_embeddings=None):
-#         super(nercLSTM, self).__init__()
-
-#         n_words = codes.get_n_words()
-#         n_sufs = codes.get_n_sufs()
-#         n_labels = codes.get_n_labels()
-        
-#         # Embeddings
-#         self.embW = nn.Embedding(n_words, 200)  # Main words
-#         self.embS = nn.Embedding(n_sufs, 100)   # Suffixes
-#         self.embL = nn.Embedding(n_words, 200)  # Lowercase words
-        
-#         # Load pretrained embeddings if provided
-#         if pretrained_embeddings is not None:
-#             self.embW.weight.data.copy_(pretrained_embeddings)
-#             self.embW.weight.requires_grad = False  # Freeze the pretrained embeddings
-
-#         # Dropout
-#         self.dropW = nn.Dropout(0.2)
-#         self.dropS = nn.Dropout(0.2)
-#         self.dropL = nn.Dropout(0.2)
-
-#         # CNN Layers
-#         self.conv1 = nn.Conv1d(in_channels=507, out_channels=256, kernel_size=3, padding=1)
-#         self.conv2 = nn.Conv1d(in_channels=256, out_channels=128, kernel_size=3, padding=1)
-#         self.conv3 = nn.Conv1d(in_channels=128, out_channels=100, kernel_size=3, padding=1)
-#         self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
-        
-#         # Transposed CNN Layer to restore sequence length
-#         self.deconv = nn.ConvTranspose1d(in_channels=100, out_channels=200, kernel_size=2, stride=2)
-
-#         # LSTM Layers
-#         self.lstm1 = nn.LSTM(200, 400, bidirectional=True, batch_first=True)
-#         self.lstm2 = nn.LSTM(800, 400, bidirectional=True, batch_first=True)
-        
-#         # Additional fully connected layer
-#         self.fc1 = nn.Linear(800, 800)
-
-#         # Output layer
-#         self.out = nn.Linear(800, n_labels)
-
-#     def forward(self, Xw, Xs, Xl, Xcap, Xdash, Xnum, Xext, Xspecial, Xlen, Xpos):
-#         xw = self.dropW(self.embW(Xw))    # Embed and apply dropout to main words
-#         xs = self.dropS(self.embS(Xs))    # Embed and apply dropout to suffixes
-#         xl = self.dropL(self.embL(Xl))    # Embed and apply dropout to lowercase words
-
-#         # Stack all features along the feature dimension
-#         x = torch.cat((xw, xs, xl, Xcap.unsqueeze(2), Xdash.unsqueeze(2), Xnum.unsqueeze(2), Xext.unsqueeze(2), Xspecial.unsqueeze(2), Xlen.unsqueeze(2), Xpos.unsqueeze(2)), dim=2)  # (batch, seq_length, feature)
-        
-#         # Permute to have channels as the second dimension for CNN layer
-#         x = x.permute(0, 2, 1)  # (batch, feature, seq_length)
-
-#         # Apply stacked CNN layers
-#         x = F.relu(self.conv1(x))
-#         x = F.relu(self.conv2(x))
-#         x = F.relu(self.conv3(x))
-#         x = self.pool(x)  # Apply pooling
-        
-#         # Restore the sequence length using transposed convolution
-#         x = self.deconv(x)  # Apply transposed CNN
-        
-#         # Permute back for LSTM layer (batch, seq_length, feature)
-#         x = x.permute(0, 2, 1)
-        
-#         x, _ = self.lstm1(x)  # First LSTM layer
-#         x, _ = self.lstm2(x)  # Second LSTM layer
-
-#         # Pass through additional fully connected layer
-#         x = F.relu(self.fc1(x))
-
-#         # Output layer
-#         x = self.out(x)       # Output layer
-        
-#         return x
-
-
-
-
-# import torch
-# import torch.nn as nn
-# from transformers import BertModel, BertTokenizer
-
-
-# class nercLSTM(nn.Module):
-#     def __init__(self, codes):
-#         super(nercLSTM, self).__init__()
-
-#         # Setting up the device
-#         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-#         # Load pre-trained BERT and move to the appropriate device
-#         self.bert = BertModel.from_pretrained('bert-base-uncased').to(self.device)
-#         for param in self.bert.parameters():
-#             param.requires_grad = False  # Freeze BERT to not train it
-
-#         n_sufs = codes.get_n_sufs()
-#         n_labels = codes.get_n_labels()
-        
-#         # Embeddings for suffixes and lowercase words
-#         self.embS = nn.Embedding(n_sufs, 50).to(self.device)    # Suffixes
-#         self.embL = nn.Embedding(codes.get_n_words(), 100).to(self.device)  # Lowercase words
-        
-#         # Dropout
-#         self.dropW = nn.Dropout(0.1)
-#         self.dropS = nn.Dropout(0.1)
-#         self.dropL = nn.Dropout(0.1)
-
-#         # LSTM Layers
-#         self.lstm1 = nn.LSTM(921, 200, bidirectional=True, batch_first=True).to(self.device)
-#         self.lstm2 = nn.LSTM(400, 200, bidirectional=True, batch_first=True).to(self.device)
-        
-#         # Output layer
-#         self.out = nn.Linear(400, n_labels).to(self.device)
-
-#     def forward(self, Xw, Xs, Xl, Xcap, Xdash, Xnum):
-#         # Move inputs to the same device as the model
-#         Xw, Xs, Xl, Xcap, Xdash, Xnum = Xw.to(self.device), Xs.to(self.device), Xl.to(self.device), Xcap.to(self.device), Xdash.to(self.device), Xnum.to(self.device)
-
-#         # Getting BERT embeddings
-#         with torch.no_grad():  # Ensure BERT is in inference mode
-#             xw = self.bert(Xw).last_hidden_state
-        
-#         xs = self.dropS(self.embS(Xs))  # Embed and apply dropout to suffixes
-#         xl = self.dropL(self.embL(Xl))  # Embed and apply dropout to lowercase words
-
-#         # Stack all features along the feature dimension
-#         x = torch.cat((xw, xs, xl, Xcap.unsqueeze(2), Xdash.unsqueeze(2), Xnum.unsqueeze(2)), dim=2)
-        
-#         x, _ = self.lstm1(x)  # First LSTM layer
-#         x, _ = self.lstm2(x)  # Second LSTM layer
-#         x = self.out(x)       # Output layer
-        
-#         return x
+#         # Combined LSTM
+#         combined_features, _ = self.lstm_combined(combined_features)
+#         combined_features = self.dropout(combined_features)  # Apply dropout
+#         fc_output = torch.relu(self.fc1(combined_features))
+#         fc_output = torch.relu(self.fc2(fc_output))
+#         fc_output = torch.relu(self.fc3(fc_output))  # Added third fully connected layer
+#         output = self.out(fc_output)
+#         return output
